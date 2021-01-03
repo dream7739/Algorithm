079be75dac21e4cd1a6d9f8c75c72f84c3fd31bf
jmzzang <dream7739@naver.com>
2021-01-04 00:00:02 +0900

    55. 기차

---
+++ 55.cpp
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
+#include <stack>
+using namespace std;
+int arr[31];
+//55. 기차운행(stack)
+
+int main(){
+    int n, i, j=1, m;
+    stack<int> s;
+    scanf("%d", &n);
+    vector<int> a(n+1); //인덱스 벡터
+    for(i=1;i<=n;i++){
+        a[i]=i;
+    }
+    vector<char> str; //push_back으로 넣으면 공간 생김
+    for(i=1;i<=n;i++){
+        scanf("%d", &m); //기차 읽기
+        s.push(m);
+        str.push_back('P');
+        while(1){
+            if(s.empty()) break; //값 없으면 break
+            if(a[j]==s.top()){
+                //s.top()이랑 a[j]랑 맞으면 계속 꺼냄
+                s.pop();
+                j++;
+                str.push_back('O');
+            }
+            else break; //같지 않으면 넘어감
+        }
+    }
+    
+    if(!s.empty()) printf("impossible");
+    else {
+        for(i=0;i<str.size();i++){
+            printf("%c", str[i]);
+        }
+    }
+    return 0;
+}
+
