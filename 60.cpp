b4a7aa8a4d61575d17e34ed24d05d0743385b261
jmzzang <dream7739@naver.com>
2021-01-09 17:01:54 +0900

    60. 합이 같은 부분집합(DFS)

---
+++ 60.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int n, a[11], total=0;
+bool flag=false;
+//60. 합이 같은 부분집합(DFS)
+
+void DFS(int L, int sum){
+    if(sum>(total/2)) return;
+    if(flag==true) return;
+    if(L==n+1){
+        if(sum==(total-sum)){
+            flag=true;
+        }
+    }
+    else{
+        DFS(L+1, sum+a[L]);
+        DFS(L+1, sum);
+    }
+}
+
+int main(){
+    int i;
+    scanf("%d", &n);
+    for(i=1;i<=n;i++){
+        scanf("%d", &a[i]);
+        total+=a[i];
+    }
+    DFS(1, 0);
+    if(flag) printf("YES\n");
+    else{ printf("NO\n");}
+    return 0;
+}
