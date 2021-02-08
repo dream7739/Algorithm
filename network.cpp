fca2d88484202d66b318bc42e51b71e4895e2896
jmzzang <dream7739@naver.com>
2021-02-08 22:37:54 +0900

    프로그래머스 - 네트워크(수정)

--- network.cpp
+++ network.cpp
@@ -6,7 +6,7 @@ int ch[201];
 void DFS(vector<vector<int>>& v, int idx){
     
     for(int i = 0; i < v[idx].size(); i++){
-        if(v[idx][i] == 1 && ch[i] == false){
+        if(v[idx][i] == 1 && ch[i] == 0){
             //연결 && 방문하지 않은 노드
             ch[i] = 1;
             DFS(v, i);
