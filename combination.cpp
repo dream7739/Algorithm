0e6935a32be0ccce0ca12d24c29c3bad0fb1ee98
jmzzang <dream7739@naver.com>
2021-03-08 21:39:24 +0900

    조합(DFS)

---
+++ combination.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+int n, r;
+int ch[20];
+
+//조합
+void DFS(int s, int L){
+    if(L==r){
+        for(int j=0;j<L;j++){
+            cout<<ch[j]<<" ";
+        }
+        cout<<endl;
+    }else{
+        for(int i=s;i<n;i++){
+            ch[L]=i;
+            DFS(i+1, L+1);
+        }
+    }
+}
+
+int main(){
+    cin>>n>>r;
+    DFS(0,0);
+    return 0;
+}
