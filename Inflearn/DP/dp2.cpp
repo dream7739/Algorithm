d1e5eabf8e3d04b300e26f31cc86a5e1c687e76c
홍정민 <dream7739@naver.com>
2024-03-16 13:06:18 +0900

    dp2: 계단 오르기(Top-Down)

---
+++ Inflearn/DP/dp2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+//2. 계단 오르기(Top-down)
+int dp[50];
+
+int DFS(int n){
+    if(n == 1 || n == 2) return n;
+    if(dp[n] > 0) return dp[n];
+    return dp[n] = DFS(n-2) + DFS(n-1);
+}
+   
+int main(){
+    int n;
+    cin >> n;
+    
+    cout << DFS(n);
+    
+    return 0;
+}
