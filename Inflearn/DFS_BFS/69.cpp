abf6a4c2dd0b83513420d58387d78b8af9a81237
홍정민 <dream7739@naver.com>
2024-03-26 13:28:52 +0900

    69: 이진트리 넓이우선탐색(BFS)

---
+++ Inflearn/DFS_BFS/69.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+//69. 이진트리 넓이우선탐색(BFS)
+int Q[100], front = -1, back = -1, ch[10];
+vector<int> map[10];
+
+int main(){
+    int a, b, x;
+    
+    for(int i = 0; i < 6; i++){
+        cin >> a >> b;
+        map[a].push_back(b);
+        map[b].push_back(a);
+    }
+    
+    Q[++back] = 1;
+    ch[1] = 1;
+    
+    while(front < back){
+        x = Q[++front];
+        cout << x << " ";
+        
+        for(int i = 0; i < map[x].size(); i++){
+            if(ch[map[x][i]] == 0){
+                ch[map[x][i]] = 1;
+                Q[++back] = map[x][i];
+            }
+        }
+    }
+    
+    return 0;
+}
