aa7dba07d838a4b7b74125c979d151c6e5016dd1
홍정민 <dream7739@naver.com>
2024-03-27 16:17:50 +0900

    74: 최소힙(STL priority_queue: 우선순위 큐)

---
+++ Inflearn/DFS_BFS/74.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+//74. 최소힙(STL priority_queue: 우선순위 큐)
+
+int main(){
+    int n;
+    priority_queue<int> pq;
+    
+    while(1){
+        cin >> n;
+        if(n == -1) break;
+        if(n == 0){
+            if(pq.empty()) cout << "-1" << "\n";
+            else{
+                cout << -pq.top() << "\n";
+                pq.pop();
+            }
+        }
+        else pq.push(-n);
+    }
+    
+    return 0;
+}
+
+
