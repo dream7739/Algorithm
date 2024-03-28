91adce43ae329b33b6a94f86e377c5f2feef2ff2
홍정민 <dream7739@naver.com>
2024-03-28 20:25:51 +0900

    삼총사

---
+++ Programmers/threeMusketeers.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> number) {
+    int answer = 0;
+    int len = number.size();
+    
+    for(int i = 0; i < len-2; i++){
+        for(int j = i+1; j < len-1; j++){
+            for(int k = j+1; k < len; k++){
+                if(number[i] + number[j] + number[k] == 0){
+                    answer += 1;
+                }
+            }
+        }
+    }
+    
+    return answer;
+}
