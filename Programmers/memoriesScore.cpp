fe3d5d9459aac4daba739010b1acc0b63909941c
홍정민 <dream7739@naver.com>
2024-03-28 19:53:00 +0900

    [해시] 추억 점수

---
+++ Programmers/memoriesScore.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include <map>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<string> name, vector<int> yearning, vector<vector<string>> photo) {
+    vector<int> answer;
+    map<string, int> m;
+    int score;
+    
+    for(int i = 0; i < name.size(); i++){
+        m.insert({name[i], yearning[i]});
+    }
+    
+    for(int i = 0; i < photo.size(); i++){
+        score = 0;
+        for(int j = 0; j < photo[i].size(); j++){
+            if(m.count(photo[i][j]) > 0){
+                score += m[photo[i][j]];
+            }
+        }
+        answer.push_back(score);
+    }
+    
+    return answer;
+}
